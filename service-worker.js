00dcebdf86eb8a2a466826e163868d5b68866f1a
MattWangTaiwan <62368399+MattWangTaiwan@users.noreply.github.com>
2020-03-21 15:15:40 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21d9945d7951fa4dbd58beb423ea742f.js"
+  "/precache-manifest.2f5dfa20685e00789a92332623fbec7e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwatrial"});

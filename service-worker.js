9c37050392232392cdd944d40b8df2f36bd26424
MattWang <mattwang@skyrec.cc>
2020-09-20 16:18:06 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.6c301d89581cbd12011da3168112446a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.34290b0b4ff9d08ebcab0f63b7626272.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute([]);
 

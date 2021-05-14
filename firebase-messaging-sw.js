a8cb78a5f6fea103075ad9a706a906c8d75d9b2f
MattWangTaiwan <62368399+MattWangTaiwan@users.noreply.github.com>
2021-05-15 00:48:13 +0800

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.10fc77f137e1950f4614d658457534f1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.79fd1377cacb5346b98bfcefc70bbd93.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute([]);
 
